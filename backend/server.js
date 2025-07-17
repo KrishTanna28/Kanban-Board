7df5600e80b4a1f8b660713c84b0c99fe7d26eec
Krish Tanna <krishttanna@gmail.com>
2025-07-17 13:43:27 +0530

    Updated Code

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,11 @@ connectDB();
 app.use('/auth', authRoutes);
 app.use('/tasks', taskRoutes);
 
+app.use((req, res, next) => {
+  console.log('Incoming request:', req.method, req.url);
+  next();
+});
+
 const server  = http.createServer(app);
 
 const io = new Server(server, {
